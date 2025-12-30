3324b1c29d524b0e1c1355e47b09f2dada24ea6c
Travis Gandy <tgandy1993@gmail.com>
2025-12-30 09:54:31 -0500

    bfs algoritm project

---
+++ breadth_first_search_algorithm.py
@@ -0,0 +1,27 @@
+"""Generate all combinations of well-formed parentheses using BFS."""
+
+
+def gen_parentheses(pairs):
+    """Function to generate all combinations of well-formed parentheses using BFS."""
+    if not isinstance(pairs, int):
+        return 'The number of pairs should be an integer'
+    if pairs < 1:
+        return 'The number of pairs should be at least 1'
+
+    queue = [('', 0, 0)]
+    result = []
+    while queue:
+        current, opens_used, closes_used = queue.pop(0)
+        if len(current) == 2 * pairs:
+            result.append(current)
+        else:
+            if opens_used < pairs:
+                queue.append((current + '(', opens_used + 1, closes_used))
+            if closes_used < opens_used:
+                queue.append((current + ')', opens_used, closes_used + 1))
+
+    return result
+
+
+print(gen_parentheses(2))
+print(gen_parentheses(3))
